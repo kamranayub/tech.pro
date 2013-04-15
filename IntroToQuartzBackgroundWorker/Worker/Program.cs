0236f74f264289e715b95fa1bd3ba308b6dabee8
Kamran Ayub <kamran.ayub@gmail.com>
2013-04-15 18:13:08 -0500

    [QuartzBackgroundWorker] Step 3: Create a scheduler

--- IntroToQuartzBackgroundWorker/Worker/Program.cs
+++ IntroToQuartzBackgroundWorker/Worker/Program.cs
@@ -1,6 +1,21 @@
-﻿namespace Worker {
+﻿using Quartz;
+using Quartz.Impl;
+
+namespace Worker {
     class Program {
+        private static readonly ISchedulerFactory SchedulerFactory;
+        private static readonly IScheduler Scheduler;
+
+        static Program() {
+
+            // Create a regular old Quartz scheduler
+            SchedulerFactory = new StdSchedulerFactory();
+            Scheduler = SchedulerFactory.GetScheduler();
+
+        }
+
         static void Main(string[] args) {
+            
         }
     }
 }
