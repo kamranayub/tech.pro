3867454d4ab8fd1536a6969d6fc2fa86e518b4f7
Kamran Ayub <kamran.ayub@gmail.com>
2013-04-15 20:35:08 -0500

    [QuartzBackgroundWorker] Step 4: Create a job

--- IntroToQuartzBackgroundWorker/Worker/Program.cs
+++ IntroToQuartzBackgroundWorker/Worker/Program.cs
@@ -5,6 +5,7 @@ namespace Worker {
     class Program {
         private static readonly ISchedulerFactory SchedulerFactory;
         private static readonly IScheduler Scheduler;
+        private static IJobDetail _emailJobDetail;
 
         static Program() {
 
@@ -16,6 +17,36 @@ namespace Worker {
 
         static void Main(string[] args) {
             
+            // Now let's start our scheduler; you could perform
+            // any processing or bootstrapping code here before
+            // you start it but it must be started to schedule
+            // any jobs
+            Scheduler.Start();
+
+            // Let's generate our email job detail now
+            CreateJob();
+        }
+
+        private static void CreateJob() {
+
+            // The job builder uses a fluent interface to
+            // make it easier to build and generate an
+            // IJobDetail object
+            _emailJobDetail = JobBuilder.Create<EmailJob>()
+                .WithIdentity("SendToMyself")   // Here we can assign a friendly name to our job        
+                .Build();                       // And now we build the job detail
+
+        }
+    }
+
+    /// <summary>
+    /// Our email job, yet to be implemented
+    /// </summary>
+    public class EmailJob : IJob {
+        public void Execute(IJobExecutionContext context) {
+
+            // TODO: Implement this
+            throw new System.NotImplementedException();
         }
     }
 }
