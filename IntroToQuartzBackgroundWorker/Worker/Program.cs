1a8a94fd0a2ecd137ec3e71a77f4878669e13843
Kamran Ayub <kamran.ayub@gmail.com>
2013-04-19 21:22:33 -0500

    [QuartzBackgroundWorker] Step 10: Integrate Mailgun

--- IntroToQuartzBackgroundWorker/Worker/Program.cs
+++ IntroToQuartzBackgroundWorker/Worker/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Configuration;
+using System.Net;
+using System.Net.Mail;
 using Quartz;
 using Quartz.Impl;
 
@@ -17,6 +19,25 @@ namespace Worker {
         /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Our email username
+        /// </summary>
+        public string EmailUsername { get; set; }
+
+        /// <summary>
+        /// Our email password
+        /// </summary>
+        public string EmailPassword { get; set; }
+
+        /// <summary>
+        /// Our email server
+        /// </summary>
+        public string EmailServer { get; set; }
+
+        /// <summary>
+        /// Our email port
+        /// </summary>
+        public int EmailPort { get; set; }
     }
 
     class Program {
@@ -68,7 +89,10 @@ namespace Worker {
 
             // Put options into data map
             _emailJobDetail.JobDataMap.Put("Email", _options.Email);
-
+            _emailJobDetail.JobDataMap.Put("Username", _options.EmailUsername);
+            _emailJobDetail.JobDataMap.Put("Password", _options.EmailPassword);
+            _emailJobDetail.JobDataMap.Put("Server", _options.EmailServer);
+            _emailJobDetail.JobDataMap.Put("Port", _options.EmailPort);
         }
 
         private static void ScheduleJob() {
@@ -128,6 +152,35 @@ namespace Worker {
             if (!String.IsNullOrEmpty(configEmail)) {
                 _options.Email = configEmail;
             }
+
+            // Try to read the email username value from app.config
+            string configEmailUsername = ConfigurationManager.AppSettings["MAILGUN_SMTP_LOGIN"];
+
+            if (!String.IsNullOrEmpty(configEmailUsername)) {
+                _options.EmailUsername = configEmailUsername;
+            }
+
+            // Try to read the email password value from app.config
+            string configEmailPassword = ConfigurationManager.AppSettings["MAILGUN_SMTP_PASSWORD"];
+
+            if (!String.IsNullOrEmpty(configEmailPassword)) {
+                _options.EmailPassword = configEmailPassword;
+            }
+
+            // Try to read the email password value from app.config
+            string configEmailServer = ConfigurationManager.AppSettings["MAILGUN_SMTP_SERVER"];
+
+            if (!String.IsNullOrEmpty(configEmailServer)) {
+                _options.EmailServer = configEmailServer;
+            }
+
+            // Try to read the email password value from app.config
+            string configEmailPort = ConfigurationManager.AppSettings["MAILGUN_SMTP_PORT"];
+            int emailPort;
+
+            if (!String.IsNullOrEmpty(configEmailServer) && Int32.TryParse(configEmailPort, out emailPort)) {
+                _options.EmailPort = emailPort;
+            }
         }
     }
 
@@ -137,11 +190,43 @@ namespace Worker {
     public class EmailJob : IJob {
         public void Execute(IJobExecutionContext context) {
 
-            // Read the value of email from our merged (final) data map
+            // Read the values from our merged (final) data map
             var email = context.MergedJobDataMap["Email"] as string;
+            var username = context.MergedJobDataMap["Username"] as string;
+            var password = context.MergedJobDataMap["Password"] as string;
+            var server = (context.MergedJobDataMap["Server"] as string) ?? "smtp.mailgun.org";
+            int port;
 
-            // TODO: Implement
-         
+            // Parse port
+            if (!Int32.TryParse(context.MergedJobDataMap["Port"] as string, out port)) {
+
+                // Default to 587
+                port = 587;
+            }
+            
+            // Ensure we have all data required
+            if (email == null || username == null || password == null) return;
+
+            // Create a new SmtpClient and dispose of it once we're done
+            // Connect to Mailgun SMTP server (they also offer a REST API)
+            // For more info, see: http://documentation.mailgun.net/quickstart.html#sending-messages
+            using (var smtpClient = new SmtpClient(server, port)) {
+
+                // Create credentials, specifying your user name and password.
+                smtpClient.Credentials = new NetworkCredential(username, password);
+                // Enable at least some security, for more info see:
+                // http://msdn.microsoft.com/en-us/library/system.net.mail.smtpclient.enablessl.aspx
+                smtpClient.EnableSsl = true;
+
+                // Send the message
+                // NOTE: We're using our Mailgun username which is also
+                // the postmaster address as the From line
+                smtpClient.Send(
+                    from: username, 
+                    recipients: email, 
+                    subject: "Test email from background worker", 
+                    body: "Hello World from our background worker!");
+            }
         }
     }
 }
